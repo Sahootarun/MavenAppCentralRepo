5520459b84b63e0e1d705770b7fdd8fcbbb622b8
tarunsahoo511@gmail.com <tarunsahoo511@gmail.com>
2020-09-30 17:44:25 +0530

    commit 3

--- GitExample/src/main/java/com/cg/project/AppMain.java
+++ GitExample/src/main/java/com/cg/project/AppMain.java
@@ -1,7 +1,13 @@
 package com.cg.project;
 
 public class AppMain {
+	
+	public static void main1(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
 
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
