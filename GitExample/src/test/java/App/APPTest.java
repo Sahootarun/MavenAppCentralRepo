96c7df578a30a7fd1b788c6fedbffc9db6860005
tarunsahoo511@gmail.com <tarunsahoo511@gmail.com>
2020-09-30 12:00:59 +0530

    test

---
+++ GitExample/src/test/java/App/APPTest.java
@@ -0,0 +1,24 @@
+package App;
+
+import org.junit.Test;
+
+public class APPTest {
+
+	@Test
+	public void test1() {
+	System.out.println("test1");
+	}
+	@Test
+	public void test2() {
+	System.out.println("test2");
+	}
+	@Test
+	public void test3() {
+	System.out.println("test3");
+	}
+	@Test
+	public void test4() {
+	System.out.println("test4");
+	}
+
+}
